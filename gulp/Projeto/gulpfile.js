2233b8a28a387c34d4665c02af9745c484269af0
Lucas Maia de Abreu <lucasmaiadeabreu@Air-de-Lucas.lan>
2023-10-12 11:47:42 +0100

    aula

--- gulp/Projeto/gulpfile.js
+++ gulp/Projeto/gulpfile.js
@@ -3,11 +3,18 @@ const concat = require('gulp-concat')
 const cssmin = require('gulp-cssmin')
 const rename = require('gulp-rename')
 const uglify = require('gulp-uglify')
+const image = require('gulp-image')
 
 
 function tarefasCss(cb){
 
-    return gulp.src('./Vendor/**/*.css')
+    return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.css',
+        './node_modules/@fortawesome/fontawesome-free/css/fontawesome.css',
+        './Vendor/Owl/owl.css',
+        './Vendor/jquery-ui/jquery-ui.css',
+        './src/css/style.css'
+    ])
        .pipe (concat('libs.css'))
        .pipe(cssmin())
        .pipe(rename({suffix:'min'}))
@@ -23,5 +30,22 @@ function tarefasJS(){
 
 }
 
+function tarefasImagem(){
+    return gulp.src('./src/Imagens/*')
+    .pipe(image({
+        pngquant: true,
+        optipng: false,
+        zopflipng: true,
+        jpegRecompress: false,
+        mozjpeg: true,
+        gifsicle: true,
+        svgo: true,
+        concurrent: 10,
+        quiet: true
+    }))
+    .pipe(gulp.dest('./dist/images'))
+}
+
 exports.styles = tarefasCss
 exports.scripts = tarefasJS
+exports.images = tarefasImagem
